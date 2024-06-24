69fe1f46f1c369db4cc78c892c7f4e2c94d8fcbf
Maxime BAUDOIN <contact@maximebaudoin.fr>
2024-06-24 08:52:48 +0200

    sync

--- test-front.js
+++ test-front.js
@@ -1,9 +1,15 @@
 const { Builder, Browser } = require("selenium-webdriver");
+const assert = require("assert");
 
 (async function helloSelenium() {
     let driver = await new Builder().forBrowser(Browser.CHROME).build();
 
-    await driver.get("https://selenium.dev");
+    await driver.get("http://localhost:5500");
+
+    const pageTitle = await driver.getTitle();
+    assert.strictEqual(pageTitle, "Calculatrice");
+
+    await driver.findElement(By.name("login")).click();
 
     await driver.quit();
 })();
